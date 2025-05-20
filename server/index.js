fff2dd54c36c259126f25cbe3c706af90530f2ac
LAU29004 <laukikwaikar@gmail.com>
2025-05-20 12:07:10 +0530

    Update server/index.js with latest changes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // Middleware to allow cross-origin requests (CORS)
 const cors = require("cors");
